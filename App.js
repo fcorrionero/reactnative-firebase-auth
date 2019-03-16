987d94c07bcc7f2d2e857e8e75169b26027042ad
Felipe Corrionero <fcorrionero@gmail.com>
2019-03-16 11:48:50 +0100

    Log out function

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './src/components/common';
+import {Button, Card, Header, Spiner} from './src/components/common';
 import { LoginForm } from './src/components/LoginForm';
 
 export default class App extends React.Component {
+
+  state = { loggedIn: null };
 
   constructor(props) {
     super(props);
@@ -16,13 +18,36 @@ export default class App extends React.Component {
       storageBucket: "",
       messagingSenderId: ""
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ loggedIn: true });
+      } else{
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+            <Button onPress={ ()=> firebase.auth().signOut() }>Log Out </Button>
+        );
+      case false:
+        return <LoginForm/>;
+      default:
+        return (
+          <Card><Spiner size="large"/></Card>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication"/>
-        <LoginForm/>
+        {this.renderContent()}
       </View>
     );
   }
